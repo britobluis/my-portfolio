5fc7be1c8ede1a8cb04b1856f4d88036e2affb6b
Luis Brito <nottluis@gmail.com>
2020-01-12 12:52:22 -0300

    Added libraries, configured styles

--- src/Components/Awards.js
+++ src/Components/Awards.js
@@ -9,7 +9,6 @@ class Awards extends Component {
 
         this.awards = props.awards;
     }
-
     render() {
         return (
             <section className="resume-section p-3 p-lg-5 d-flex align-items-center" id="awards">
