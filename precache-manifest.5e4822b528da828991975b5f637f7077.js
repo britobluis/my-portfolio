7c8fa3a67958299ad1f90e1f25384d49b733f0d6
Luis Brito <nottluis@gmail.com>
2020-01-12 16:24:38 -0300

    Updates

---
+++ precache-manifest.5e4822b528da828991975b5f637f7077.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "db38b8c28d9e2c9c6f5dbe9073b8cc10",
+    "url": "/index.html"
+  },
+  {
+    "revision": "85e28ced5cc3ae53b553",
+    "url": "/static/css/main.69eda509.chunk.css"
+  },
+  {
+    "revision": "d6131afa395931d8af0f",
+    "url": "/static/js/2.8a849342.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.8a849342.chunk.js.LICENSE"
+  },
+  {
+    "revision": "85e28ced5cc3ae53b553",
+    "url": "/static/js/main.31b4b759.chunk.js"
+  },
+  {
+    "revision": "b6d641d7babc028ccdf2",
+    "url": "/static/js/runtime-main.712fe5d1.js"
+  },
+  {
+    "revision": "f42188e435dc37e69bc29bd210dbb740",
+    "url": "/static/media/profile.f42188e4.jpg"
+  }
+]);
